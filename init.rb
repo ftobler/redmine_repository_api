958999884f62c7ea8588b2aaae55a401b6000a32
Florin Tobler <florin.tobler@hotmail.com>
2026-03-01 15:41:22 +0100

    plugin description

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 
 
 Redmine::Plugin.register :redmine_repository_api do
-  name 'Redmine Repository API extension'
+  name 'Redmine Repository API'
   author 'Florin Tobler'
   author_url 'https://github.com/ftobler'
-  description 'Plugin Template. Does nothing.'
+  description 'Exposes a REST API endpoint listing projects and their repositories.'
   url 'https://github.com/ftobler/redmine_repository_api'
   version '0.0.1'
   requires_redmine :version_or_higher => '6.1.0'
